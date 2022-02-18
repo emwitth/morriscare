97d01e1cf2472362b7c5176ba2a06ea66f2beb38
Evan Witthun <98661203+emwitth@users.noreply.github.com>
2022-02-17 18:32:22 -0600

    add staff member interface

---
+++ frontend/src/app/interfaces/StaffMember.ts
@@ -0,0 +1,12 @@
+/**
+ * Interface describing a single staff member.
+ */
+
+export interface StaffMember {
+    firstName: string,
+    lastName: string,
+    postalAddress: string,
+    email: string,
+    phoneNumber: string
+  }
+  
