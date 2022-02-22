86479c7565c098f8a859a9cc4c439549cab00119
Evan Witthun <98661203+emwitth@users.noreply.github.com>
2022-02-22 09:21:55 -0600

    add ct integration

--- frontend/src/app/dialog-components/add-ct-dialog/register-ct-dialog.component.ts
+++ frontend/src/app/dialog-components/add-ct-dialog/register-ct-dialog.component.ts
@@ -2,6 +2,7 @@ import { Component, Inject, OnInit, Optional } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { FormattingModule } from 'src/app/modules/formatting/formatting.module';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-register-ct-dialog',
@@ -17,6 +18,7 @@ export class RegisterCtDialogComponent implements OnInit {
   constructor(
     private fb: FormBuilder,
     private format: FormattingModule,
+    private http: HttpClient,
     public dialogRef: MatDialogRef<RegisterCtDialogComponent>,
     @Optional() @Inject(MAT_DIALOG_DATA) public mydata: any
   ) {
@@ -24,7 +26,7 @@ export class RegisterCtDialogComponent implements OnInit {
       firstName: ['', [Validators.required, Validators.pattern('[a-zA-Z]*')]],
       lastName: ['', [Validators.required, Validators.pattern('[a-zA-Z]*')]],
       email: ['', [Validators.required, Validators.email]],
-      address: ['', [Validators.required, Validators.pattern('[a-zA-Z0-9]*')]],
+      address: ['', [Validators.required, Validators.pattern('[a-zA-Z0-9 ]*')]],
       phone: ['', [Validators.required, Validators.pattern('[0-9]{10}')]]
     }, {});
    }
@@ -48,6 +50,30 @@ export class RegisterCtDialogComponent implements OnInit {
     console.log(this.form.get('address')?.value);
     console.log(this.form.get('phone')?.value);
     console.log(this.format.formatPhone(this.form.get('phone')?.value));
-    this.dialogRef.close({ event: 'close', data: true }); 
+     
+    var body = {
+      firstName: this.form.get('firstName')?.value,
+      lastName: this.form.get('lastName')?.value,
+      email: this.form.get('email')?.value,
+      postalAddress: this.form.get('address')?.value,
+      phoneNumber: this.form.get('phone')?.value,
+      role: "staff"
+    }
+
+    // console.log(body);
+    this.http.post<any>("api/caretakers/", body, { observe: "response" }).subscribe(result => {
+      // console.log(result.body);
+      if (result.status != 200) {
+        // this.isIncorrectLogin = true;
+      } else if(result.status == 200) {
+        // console.log(result.body);
+        this.dialogRef.close({ event: 'close', data: true }); 
+      }
+    }, err => {
+      // this.isIncorrectLogin = true;
+    });
+
+    // this.dialogRef.close({ event: 'close', data: true }); 
   }
+
 }
