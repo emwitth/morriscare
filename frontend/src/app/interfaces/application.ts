2f5a428307f69948adb218040b505f110a323755
Evan Witthun <98661203+emwitth@users.noreply.github.com>
2022-03-02 13:31:47 -0600

    added temp get request, will use when backend done

---
+++ frontend/src/app/interfaces/application.ts
@@ -0,0 +1,10 @@
+/**
+ * Interface describing a single job posting
+ */
+
+export interface Application {
+  type: string,
+  qualifications: string,
+  education: string
+}
+  
--- frontend/src/app/job-list/job-list.component.ts
+++ frontend/src/app/job-list/job-list.component.ts
@@ -1,5 +1,8 @@
+import { Application } from './../interfaces/application';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { SnackbarModule } from '../modules/snackbar/snackbar.module';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-job-list',
@@ -7,12 +10,26 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./job-list.component.css']
 })
 export class JobListComponent implements OnInit {
+  jobs: Array<Application> = new Array<Application>();
   type: string = "";
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private snackbar: SnackbarModule,
+    private http: HttpClient) { }
 
   ngOnInit(): void {
     this.type = this.route.snapshot.params?.type;
+
+    // this.http.get<any>("api/applications?type=" + this.type.charAt(0), { observe: "response" }).subscribe(result => {
+    //   if (result.status != 200) {
+    //     this.snackbar.openSnackbarErrorCust("Failed to fetch job postings for " + this.type);
+    //   } else if(result.status == 200) {
+    //     result.body.forEach((element: Application) => {
+    //       this.jobs.push(element);
+    //     });
+    //   }
+    // }, err => {
+    //   this.snackbar.openSnackbarErrorCust("Failed to fetch job postings for " + this.type);
+    // });
   }
 
 }
