7e6f15a3535845916f2324fe9157d23b5180c01c
Evan Witthun <98661203+emwitth@users.noreply.github.com>
2022-03-02 09:34:42 -0600

    add temp submit function, finish in integration

--- frontend/src/app/hcp-application/hcp-application.component.ts
+++ frontend/src/app/hcp-application/hcp-application.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-hcp-application',
@@ -16,7 +17,7 @@ export class HcpApplicationComponent implements OnInit {
 
   today: Date = new Date();
 
-  constructor(private route: ActivatedRoute, private fb: FormBuilder) { 
+  constructor(private route: ActivatedRoute, private fb: FormBuilder, private http: HttpClient) { 
     this.form = this.fb.group({
       first: ['', [Validators.required, Validators.pattern("[A-Za-z]*")]],
       last: ['', [Validators.required, Validators.pattern("[A-Za-z]*")]],
@@ -31,7 +32,41 @@ export class HcpApplicationComponent implements OnInit {
     }, {});
   }
 
-  submit(){}
+  submit(){
+    console.log("first", this.form.get("first")?.value);
+    console.log("last", this.form.get("last")?.value);
+    console.log("phone", this.form.get("phone")?.value);
+    console.log("email", this.form.get("email")?.value);
+    console.log("sex", this.form.get("sex")?.value);
+    console.log("address", this.form.get("address")?.value);
+    console.log("dob", this.form.get("dob")?.value);
+    console.log("ssn", this.form.get("ssn")?.value);
+    console.log("experience", this.form.get("experience")?.value);
+    console.log("qualifications", this.form.get("qualifications")?.value);
+
+    var body = {
+      firstName: this.form.get("first")?.value,
+      lastName: this.form.get("last")?.value,
+      email: this.form.get("email")?.value,
+      sex: this.form.get("sex")?.value,
+      phoneNumber: this.form.get("phone")?.value,
+      address: this.form.get("address")?.value,
+      dateOfBirth: this.form.get("dob")?.value,
+      ssn: this.form.get("ssn")?.value,
+      yearsOfExperience: this.form.get("experience")?.value,
+      qualifications: this.form.get("qualifications")?.value
+    }
+    
+    // this.http.post<any>("api/application/" + this.id + "/applicant", body, { observe: "response" }).subscribe(result => {
+    //   if (result.status != 200) {
+    //     //
+    //   } else if(result.status == 200) {
+
+    //   }
+    // }, err => {
+    //   //
+    // });
+  }
 
   ngOnInit(): void {
     this.id = this.route.snapshot.params?.id;
