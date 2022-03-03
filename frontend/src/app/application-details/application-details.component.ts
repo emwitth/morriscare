12037120345e1bd6e34b9a5e0ba38b90699acce6
Evan Witthun <98661203+emwitth@users.noreply.github.com>
2022-03-03 17:35:57 -0600

    pass description information to details

--- frontend/src/app/application-details/application-details.component.ts
+++ frontend/src/app/application-details/application-details.component.ts
@@ -8,10 +8,17 @@ import { Roles } from '../global-variables';
 })
 export class ApplicationDetailsComponent implements OnInit {
   userType: string = "";
+  qualifications: string = "";
+  education: string = "";
 
   constructor() { }
 
   ngOnInit(): void {
+    console.log(history.state.data);
+
+    this.qualifications = history.state.data.qualifications;
+    this.education = history.state.data.education;
+
     if(Roles.admin == sessionStorage.getItem("role")) {
       this.userType = "admin";
     }
