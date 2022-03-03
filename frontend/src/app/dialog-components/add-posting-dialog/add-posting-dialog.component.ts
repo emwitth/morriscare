529c07a26b085f027542bd697fb996d7785f965d
Evan Witthun <98661203+emwitth@users.noreply.github.com>
2022-03-03 10:29:22 -0600

    add temp post, will update when backend done

--- frontend/src/app/dialog-components/add-posting-dialog/add-posting-dialog.component.ts
+++ frontend/src/app/dialog-components/add-posting-dialog/add-posting-dialog.component.ts
@@ -2,6 +2,7 @@ import { Component, Inject, OnInit, Optional } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
+import { SnackbarModule } from 'src/app/modules/snackbar/snackbar.module';
 
 @Component({
   selector: 'app-add-posting-dialog',
@@ -15,6 +16,7 @@ export class AddPostingDialogComponent implements OnInit {
   form: FormGroup;
 
   constructor(private fb: FormBuilder,private http: HttpClient,
+    private snackbar: SnackbarModule,
     public dialogRef: MatDialogRef<AddPostingDialogComponent>,
     @Optional() @Inject(MAT_DIALOG_DATA) public mydata: any
     ) {
@@ -25,7 +27,28 @@ export class AddPostingDialogComponent implements OnInit {
       }, {});
     }
 
-  create(){}
+  create(){
+    console.log(this.form.get("Type")?.value);
+    console.log(this.form.get("Qualifications")?.value);
+    console.log(this.form.get("Education")?.value);
+    // var body = {
+    //   type: this.form.get("Type")?.value,
+    //   qualifications: this.form.get("Qualifications")?.value,
+    //   education: this.form.get("Education")?.value,
+    // }
+
+    // this.http.post<any>("api/auth/", body, { observe: "response" }).subscribe(result => {
+    //   // console.log(result.body);
+    //   if (result.status != 200) {
+    //     this.snackbar.openSnackbarErrorCust("Failed to submit new application. Please try again.")
+    //   } else if(result.status == 200) {
+    //     this.snackbar.openSnackbarSuccessCust("New Applicaion Successfully Submitted!");
+    //     this.dialogRef.close({ event: 'close', data: false });
+    //   }
+    // }, err => {
+    //   this.snackbar.openSnackbarErrorCust("Failed to submit new application. Please try again.")
+    // });
+  }
 
   // close dialogue with false state
   closeDialog() { this.dialogRef.close({ event: 'close', data: false }); }
