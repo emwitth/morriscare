73207e31b1edc4e68ba2e5d0b0489ca219eb8647
Evan Witthun <98661203+emwitth@users.noreply.github.com>
2022-03-03 10:17:28 -0600

    add form to dialog

--- frontend/src/app/dialog-components/add-posting-dialog/add-posting-dialog.component.ts
+++ frontend/src/app/dialog-components/add-posting-dialog/add-posting-dialog.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, Optional } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-add-posting-dialog',
@@ -6,8 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-posting-dialog.component.css']
 })
 export class AddPostingDialogComponent implements OnInit {
+  fromPage!: string;
+  fromDialog!: string;
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder,private http: HttpClient,
+    public dialogRef: MatDialogRef<AddPostingDialogComponent>,
+    @Optional() @Inject(MAT_DIALOG_DATA) public mydata: any
+    ) {
+      this.form = this.fb.group({
+        Type: ['', Validators.required],
+        Qualifications: ['', [Validators.required, Validators.pattern("[A-Za-x0-9 ]*")]],
+        Education: ['', [Validators.required, Validators.pattern("[A-Za-x0-9 ]*")]]
+      }, {});
+    }
+
+  create(){}
+
+  // close dialogue with false state
+  closeDialog() { this.dialogRef.close({ event: 'close', data: false }); }
 
   ngOnInit(): void {
   }
