fe17a1bfcf6bd2239e8564b97aed7bfb4e9c14da
Evan Witthun <98661203+emwitth@users.noreply.github.com>
2022-03-06 15:32:43 -0600

    application delete integration

--- frontend/src/app/application-details/application-details.component.ts
+++ frontend/src/app/application-details/application-details.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Roles } from '../global-variables';
 import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { SnackbarModule } from '../modules/snackbar/snackbar.module';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-application-details',
@@ -13,13 +16,12 @@ export class ApplicationDetailsComponent implements OnInit {
   education: string = "unavailable";
   type: string = "unavailable";
   experience: string = "unavailable";
-  APPID: number = 1;
+  id: number = 1;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private http: HttpClient, 
+    private snackbar: SnackbarModule, private router: Router) { }
   ngOnInit(): void {
-    console.log(history.state.data);
-    console.log("details", this.route.snapshot.params);
-    this.APPID = this.route.snapshot.params?.appId;
+    this.id = this.route.snapshot.params?.id;
 
     this.qualifications = history.state.data.qual;
     this.education = history.state.data.ed;
@@ -32,6 +34,21 @@ export class ApplicationDetailsComponent implements OnInit {
     else if(Roles.sm == sessionStorage.getItem("role")) {
       this.userType = "staff";
     }
+  }
+
+  remove() {
+    this.http.delete<any>("api/application/" + this.id + "/", { observe: "response" }).subscribe(result => {
+      console.log(result);
+      if (result.status != 200) {
+        this.snackbar.openSnackbarError();
+      } else if(result.status == 200) {
+        console.log(result.body);
+        this.router.navigate(["/" + this.userType + "/applications"]);
+        this.snackbar.openSnackbarSuccessCust("Application " + this.id + " successfully deleted!");
+      }
+    }, err => {
+      this.snackbar.openSnackbarErrorCust(err.error.error);
+    });
   }
 
 }
