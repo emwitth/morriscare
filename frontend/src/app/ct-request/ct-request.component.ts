e3cdacbf9d986b01f892a3a8d4eed55bac9c5403
Evan Witthun <98661203+emwitth@users.noreply.github.com>
2022-03-17 21:14:49 -0500

    intial initial request form

--- frontend/src/app/ct-request/ct-request.component.ts
+++ frontend/src/app/ct-request/ct-request.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-ct-request',
@@ -7,7 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CtRequestComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  today: Date = new Date();
+
+  numberOfDays:number = 10;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      sex: ['', Validators.required],
+      dob: ['', Validators.required],
+      location: ['', Validators.required],
+      phone: ['', [Validators.required, Validators.pattern('[0-9]*')]],
+      email: ['', [Validators.required, Validators.email]],
+      type: ['', Validators.required],
+      startDate: ['', Validators.required],
+      endDate: ['', Validators.required],
+      startTime: ['', Validators.required],
+      endTime: ['', Validators.required],
+      gender: ['', Validators.required],
+    });
+  }
 
   ngOnInit(): void {
   }
