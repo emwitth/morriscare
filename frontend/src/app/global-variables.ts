0c16bcced791ce239ba51636372a7e18add2d149
Evan Witthun <98661203+emwitth@users.noreply.github.com>
2022-03-19 14:24:24 -0500

    add global values - requirement dropdown

--- frontend/src/app/global-variables.ts
+++ frontend/src/app/global-variables.ts
@@ -6,4 +6,23 @@ export const enum Roles {
     sm = 'staff',
     ct = 'ct',
     hcp = 'hcp'
-}
+};
+
+/**
+ * These are the choices for hiring requirement dropdowns
+ */
+export const hiringRequirements: Array<string> = [
+    "Test qualification",
+    "Another qualification",
+    "A very qualified individual"
+];
+
+/**
+ * These are the choices for hiring education dropdowns
+ */
+ export const hiringEducation: Array<string> = [
+    "Bachelor's",
+    "Master's",
+    "Doctorate",
+    "Other"
+];
