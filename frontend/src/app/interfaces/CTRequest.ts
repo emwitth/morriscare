7a789617f9dafac16be8472fd183f5d9a067791b
Evan Witthun <98661203+emwitth@users.noreply.github.com>
2022-03-19 17:59:45 -0500

    add interface - sm ct request list

---
+++ frontend/src/app/interfaces/CTRequest.ts
@@ -0,0 +1,28 @@
+/**
+ * Interface describing a single care taker request
+ */
+
+export interface Requirements {
+  serviceType: string,
+  daysRequested: number,
+  startDate: string,
+  endDate: string,
+  [key: string]: any
+}
+
+export interface CTRequest {
+  requestID: number,
+  patientFirstName: string,
+  patientLastName: string,
+  sex: string,
+  dateOfBirth: string,
+  locationOfService: string,
+  patientPhoneNumber: string,
+  patientEmail: string,
+  serviceType: string,
+  deleted: boolean,
+  hcpID: number,
+  userID: number,
+  requirements: Requirements
+}
+  
