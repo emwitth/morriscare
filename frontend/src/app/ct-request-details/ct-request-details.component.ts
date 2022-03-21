f6914c950b2fec7a5ae097822d4033ea1674d7b9
Evan Witthun <98661203+emwitth@users.noreply.github.com>
2022-03-20 20:24:18 -0500

    added informational pieces - sm ct request details

--- frontend/src/app/ct-request-details/ct-request-details.component.ts
+++ frontend/src/app/ct-request-details/ct-request-details.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { SnackbarModule } from '../modules/snackbar/snackbar.module';
 import { ActivatedRoute } from '@angular/router';
+import { FormattingModule } from '../modules/formatting/formatting.module';
 import { CTRequest, Requirements } from '../interfaces/CTRequest';
+import { Roles } from '../global-variables';
 
 @Component({
   selector: 'app-ct-request-details',
@@ -12,9 +14,13 @@ import { CTRequest, Requirements } from '../interfaces/CTRequest';
 export class CtRequestDetailsComponent implements OnInit {
   request!: CTRequest;
   id = this.route.snapshot.params?.id;
+  userType: string = "";
+  isFlexibleHours: boolean = false;
+  wantsGender: boolean = false;
+  wantsAge: boolean = false;
 
   constructor( private http: HttpClient, private snackbar: SnackbarModule, 
-    private route: ActivatedRoute ) { }
+    private route: ActivatedRoute, public format: FormattingModule ) { }
 
   ngOnInit(): void {
     // retrieve a single request
@@ -23,10 +29,28 @@ export class CtRequestDetailsComponent implements OnInit {
         this.snackbar.openSnackbarErrorCust("Failed to fetch request " + this.id + ": status " + result.status);
       } else if(result.status == 200) {
         this.request = result.body;
+        if(this.request.requirements?.hoursPerDay != null) {
+          this.isFlexibleHours = true;
+        }
+        if(this.request.requirements?.minAge != null 
+        && this.request.requirements?.maxage != null) {
+          this.wantsAge = true;
+        }
+        if(this.request.requirements?.gender != null) {
+          this.wantsGender = true;
+        }
       }
     }, err => {
       this.snackbar.openSnackbarErrorCust("Failed to fetch request " + this.id + ":" + err.error.error);
     });
+
+    // set role of current user for correct navigation back to /role/applications
+    if(Roles.admin == sessionStorage.getItem("role")) {
+      this.userType = "admin";
+    }
+    else if(Roles.sm == sessionStorage.getItem("role")) {
+      this.userType = "staff";
+    }
   }
 
 }
