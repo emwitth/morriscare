3a4ca818702bbdad3aac453c61042666b7d81c7f
Evan Witthun <98661203+emwitth@users.noreply.github.com>
2022-04-07 12:08:16 -0500

    added general layout - hcp schedule

--- frontend/src/app/hcp-schedule/hcp-schedule.component.ts
+++ frontend/src/app/hcp-schedule/hcp-schedule.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HcpScheduleComponent implements OnInit {
 
+  selected!: Date | null;
+
   constructor() { }
 
   ngOnInit(): void {
