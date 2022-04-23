38c7a74c0d9f7ab3397bf5b03b92e2fe585617ec
Evan Witthun <98661203+emwitth@users.noreply.github.com>
2022-04-23 15:03:47 -0500

    disable some fields when they should be lol

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -71,7 +71,6 @@ import { CtRequestDetailsComponent } from './ct-request-details/ct-request-detai
 import { CtRequestHcpComponent } from './ct-request-hcp/ct-request-hcp.component';
 import { CtRequestCtViewComponent } from './ct-request-ct-view/ct-request-ct-view.component';
 import { HcpScheduleComponent } from './hcp-schedule/hcp-schedule.component';
-import { BillingAccountSmViewComponent } from './billing-account-sm-view/billing-account-sm-view.component';
 import { BillingAccountCtViewComponent } from './billing-account-ct-view/billing-account-ct-view.component';
 import { HcpWageViewComponent } from './hcp-wage-view/hcp-wage-view.component';
 
@@ -108,7 +107,6 @@ import { HcpWageViewComponent } from './hcp-wage-view/hcp-wage-view.component';
     CtRequestHcpComponent,
     CtRequestCtViewComponent,
     HcpScheduleComponent,
-    BillingAccountSmViewComponent,
     BillingAccountCtViewComponent,
     HcpWageViewComponent,
   ],
